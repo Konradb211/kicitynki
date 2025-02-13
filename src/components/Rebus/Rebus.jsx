f507e04bd67849d4edf6e46a34f38d16651e337d
Intelli App <dev@intelliapp.pl>
2025-02-14 00:51:24 +0100

    trzecia

--- src/components/Rebus/Rebus.jsx
+++ src/components/Rebus/Rebus.jsx
@@ -5,8 +5,8 @@ const Rebus = () => {
 		<>
 			<h1>Na koniec mały rebus(mega trudny)</h1>
 			<div className={styles.images}>
-				<img src='/public/potas.jpeg' alt='Potas' />
-				<img src='/public/wegiel.webp' alt='Węgiel' />
+				<img src='/potas.jpeg' alt='Potas' />
+				<img src='/wegiel.webp' alt='Węgiel' />
 			</div>
 		</>
 	)
